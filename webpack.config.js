543eeba5ceeb01e0d92c117371a42a3aa681bf72
zhen <kelly811124811124@hotmail.com>
2020-08-21 15:41:02 +0800

    set index config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,13 @@
+// 把相對路徑組成絕對路徑
+var path = require('path');
+// dirname 當前的絕對路徑
+console.log('dirname:', __dirname)
+// 避免環境變了 路徑寫死會掛掉 所以拿到當前路徑來組相對路徑
+console.log(path.resolve(__dirname, "./src"));
 module.exports = {
+    // 指定資料夾
+    context: path.resolve(__dirname, "./src"),
+    // 進入點
     entry: './index.js',
     output: {
         filename: 'index-bundle.js'
