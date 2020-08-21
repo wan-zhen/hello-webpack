9470b29cf6be530cb20f0a0accd7fb6b6d08fabf
zhen <kelly811124811124@hotmail.com>
2020-08-21 15:57:50 +0800

    set mode

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,8 @@ console.log('dirname:', __dirname)
 // 避免環境變了 路徑寫死會掛掉 所以拿到當前路徑來組相對路徑
 console.log(path.resolve(__dirname, "./src"));
 module.exports = {
+    // set mode 'development'(執行速度較快) or 'production' (default 會優化、壓縮)
+    mode: 'development',
     // 指定資料夾
     context: path.resolve(__dirname, "./src"),
     // 進入點
