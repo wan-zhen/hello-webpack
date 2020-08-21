99417fb6f57935a12eb5b42acec648c14cbeb7f9
zhen <kelly811124811124@hotmail.com>
2020-08-21 16:08:44 +0800

    set script use NODE_ENV set mode

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,11 @@ var path = require('path');
 console.log('dirname:', __dirname)
 // 避免環境變了 路徑寫死會掛掉 所以拿到當前路徑來組相對路徑
 console.log(path.resolve(__dirname, "./src"));
+// windows install cross-env get NODE_ENV，並進一步取得 npm script 設定的值
+console.log(process.env.NODE_ENV)
 module.exports = {
     // set mode 'development'(執行速度較快) or 'production' (default 會優化、壓縮)
-    // mode: 'development',
+     mode: process.env.NODE_ENV,
     // 指定資料夾
     context: path.resolve(__dirname, "./src"),
     // 進入點
