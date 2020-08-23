b560270394b7d928cdcdedc1c16e7f5ab3e4e33f
zhen <kelly811124811124@hotmail.com>
2020-08-23 17:25:32 +0800

    use module.exports & require

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,13 @@ import "../index.html";
 // 新增一些 ES6 的語法
 let testArr = [1, 2, 3];
 testArr.map((obj) => console.log(obj));
+
+var test = require('./testModule.js');
+// will get {name: "zhen"}
+console.log(test)
+
+var test1 = require('./testModule1.js');
+// will console function
+console.log(test1);
+// 可以直接使用
+test1('zhen');
---
+++ src/js/testModule1.js
@@ -0,0 +1,5 @@
+// 可以用 module.exports 把 function 模組化
+// 外面要用的人可以直接 require
+module.exports = function (name) {
+    console.log(name)
+}
